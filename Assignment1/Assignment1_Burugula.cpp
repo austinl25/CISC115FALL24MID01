1205069550847dbcba3905a6d404b9c4e6aeaa42
KameronPM08 <kameron.martinez@student.northampton.edu>
2024-10-05 02:46:37 +0000

    DONE

--- Assignment1/Assignment1_Burugula.cpp
+++ Assignment1/Assignment1_Burugula.cpp
@@ -2,5 +2,5 @@
 using namespace std;
 int main(){
 
-    cout<<"My first program for the semester"<<endl;
+return 0;
 }
---
+++ Assignment1/Assignment1_MartinezUPDATED.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // Integer Example using "Whole numbers"
+int value = 2;
+int speed = 15;
+int year = 2024;
+int a = 5;
+
+// Character example using "Singular Character"
+char Lastinitial = 'A'; 
+char Fristinitial = 'B';
+char currency = '$';
+char b = '2';
+
+// Double example using "Decimal values"
+double price = 7.99;
+double weight = 168.8;
+double distance = 14.9;
+
+// Boolean example "Represents true or false state"
+bool on = true;
+bool off = false;
+bool correct = true;
+
+// String example "Sequence of text representing an object"
+std::string fullname = "MyName";
+std::string Job_Description = "worker";
+std::string City = "Easton";
+
+//Float example "A floating decimal place value"
+float big = 1.9;
+float samll = 0.9;
+float size = 1.5;
+
+// Simple algorithm operation
+size = size + 10;
+
+// Implicit cast = Auto conversion 
+int x = 3.14;
+// "int" can only hold whole numbers and will automaticlly convert decimal value to nearest integer when displayed resulting in "3".
+    
+//Explicit conversion = changing a pre-exsiting varable to represent another data type.
+double y = (int) 3.14;
+// Here we specified that we want to convert the double value of 3.14 to an integer value explicitly, giving us the value of "3"
+
+
+
+
+
+// Code input should look like this for proper output for assignment
+    
+        int age = 23;
+    double height = 5.8;
+std:: string name = "Kameron M.";
+char grade = 'A';
+
+std::cout << "Hello my name is " << name; '\n';
+std::cout << " I am " << age; '\n';
+std::cout << " I am also " << height; 
+std::cout << " Coding is fun to me haha so I deserve an " << grade;
+ 
+
+ // Sorry, I really overdid the assignment because I thought we had to do more then just this.
+    
+    return 0;
+}
