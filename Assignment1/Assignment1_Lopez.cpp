a38ffb5b088a2d6d0f605a934921b7a221361454
austinl25 <austinlopez25@gmail.com>
2024-10-05 12:52:41 +0000

    Coding Assignment1

---
+++ Assignment1/Assignment1_Lopez.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main(){
+    std::string name = "Austin";
+    //Applied string for name
+
+    std::cout << "Name: " << name << '\n';
+    //Standard ouput for label the name string
+
+    std::cout << "This is my first basic program in c++" << '\n';
+
+    int x = 10;
+
+    int y = 16;
+
+    int sum = x + y;
+
+    int age = 26;
+
+    double height = 5.9;
+
+    char grade = 'A';
+
+    //Created different integers and character, Doubling the height to add the decimal .9
+
+    std::cout << "Grade I want in CISC115: " << grade << '\n';
+
+    std::cout << "Height: " << height << '\n';
+
+    std::cout << "Age: " << sum << '\n';
+
+    age = age + 15;
+
+    std::cout << "New age: " << age << '\n';
+}
